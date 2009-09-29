2ff62751838f3aee2d6f1b8696a790cea3a10d68
Dykam <kramieb@gmail.com>
2009-09-29 21:42:17 +0200

    Done BinaryTree. Looks solid but iteration and stuff is still missing.
    Signed-off-by: Dykam <kramieb@gmail.com>

---
+++ haxe/more/data/structures/BinaryTree.hx
@@ -0,0 +1,25 @@
+﻿/**
+ * ...
+ * @author Mark
+ */
+
+package haxe.more.data.structures;
+
+typedef Comperator < T > = T -> T -> Int;
+
+class BinaryTree<T> extends BinaryTreeNode<T> {
+	static var init = (function() {
+		left = -1;
+	})();
+	
+	public static var left(default, null):Int;
+	public static function integerComparator(child:Int, ancestor:Int):Int {
+		return child - ancestor;
+	}
+	
+	public var comperator(default, default):Comperator<T>;
+	public function new(value:T, comperator:Comperator<T>) {
+		super(value);
+		this.comperator = comperator;
+	}
+}
---
+++ haxe/more/data/structures/BinaryTreeNode.hx
@@ -0,0 +1,63 @@
+﻿/**
+ * ...
+ * @author Mark
+ */
+
+package haxe.more.data.structures;
+import flash.Error;
+
+class BinaryTreeNode<T> {
+	public var left(default, null):BinaryTreeNode<T>;
+	public var right(default, null):BinaryTreeNode<T>;
+	public var value(default, null):T;
+	public var root(default, null):BinaryTree<T>;
+	
+	function new(value:T) this.value = value
+	
+	public function insert(value:T):BinaryTreeNode<T> {
+		var created = new BinaryTreeNode(value);
+		var comperator = root.comperator;
+		var current = this;
+		while (current.left != null || current.right != null) {
+			var contrast = comperator(value, this.value);
+			if (contrast < 0) {
+				if (left == null) {
+					left = created;
+					break;
+				}
+				else {
+					current = left;
+				}
+			} else if(contrast > 0) {
+				if (right == null) {
+					right = created;
+					break;
+				}
+				else {
+					current = right;
+				}
+			} else {
+				var leftDistance = comperator(value, left.value);
+				var rightDistance = comperator(value, right.value);
+				if (leftDistance < rightDistance) {					
+					if (left == null) {
+						left = created;
+						break;
+					}
+					else {
+						current = left;
+					}
+				} else {
+					if (right == null) {
+						right = created;
+						break;
+					}
+					else {
+						current = right;
+					}
+				}
+			}
+		}
+		return created;
+	}
+}
