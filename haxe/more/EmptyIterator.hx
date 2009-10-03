33b467c652d6758552eb97c07690a61af80ea341
Mark de Bruijn <kramieb@gmail.com>
2009-10-03 09:28:42 +0200

    And the needed EmptyIterator. Doesn't iterate anything.

---
+++ haxe/more/EmptyIterator.hx
@@ -0,0 +1,12 @@
+﻿/**
+ * ...
+ * @author Dykam
+ */
+
+package haxe.more;
+
+class EmptyIterator < T > {
+	public function new();
+	public function hasNext() return false
+	public function next():T return null
+}
